b65b923fc03fbff6dc4195f2031deb4e145e592b
Harsh.Dronapay <harsh@dronapay>
2025-01-08 07:23:58 +0000

    upadtes

--- main.tf
+++ main.tf
@@ -6,43 +6,31 @@ resource "google_compute_network" "vpc_network" {
 }
 
 resource "google_compute_subnetwork" "subnetwork" {
-  name                    = "subnetwork"
-  ip_cidr_range           = "10.0.0.0/16"
-  region                  = "asia-south1"
-  network                 = google_compute_network.vpc_network.id
-  private_ip_google_access = true  # Enable Private Google Access
+  name                     = "subnetwork"
+  ip_cidr_range            = "10.0.0.0/16"
+  region                   = "asia-south1"
+  network                  = google_compute_network.vpc_network.id
+  private_ip_google_access = true
 }
 
 resource "google_container_cluster" "htc_argo" {
   name     = "htc-argo"
   location = "asia-south1-a"
 
-  # Use a single node to minimize costs
   initial_node_count = 1
 
-  # Node configuration
   node_config {
-    # Cheapest machine type
     machine_type = "e2-micro"
-
-    # Minimum disk size (10 GB is the smallest allowed size)
     disk_size_gb = 10
-
-    # Use standard persistent disk for lower cost
-    disk_type = "pd-standard"
-
-    # Use the default service account
+    disk_type    = "pd-standard"
     oauth_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
   }
 
-  # Use the VPC network and subnetwork
   network    = google_compute_network.vpc_network.id
   subnetwork = google_compute_subnetwork.subnetwork.id
 
-  # Disable various addons to reduce costs
   remove_default_node_pool = true
 
-  # Enable only essential features under the addons_config block
   addons_config {
     http_load_balancing {
       disabled = true
@@ -52,33 +40,29 @@ resource "google_container_cluster" "htc_argo" {
     }
   }
 
-  # Enable private nodes
   private_cluster_config {
-    enable_private_nodes    = true  # Equivalent to --enable-private-nodes
+    enable_private_nodes    = true
     enable_private_endpoint = false
   }
 
-  # Enable IP allocation policy
   ip_allocation_policy {
     cluster_ipv4_cidr_block = "/14"
   }
 
-  # Set deletion protection to false
   deletion_protection = false
 }
 
 resource "google_container_node_pool" "cheap_pool" {
   cluster    = google_container_cluster.htc_argo.id
   location   = google_container_cluster.htc_argo.location
-  node_count = 1 # Keep node count minimal
+  node_count = 1
 
   node_config {
-    machine_type = "e2-micro" # Smallest and cheapest machine type
-    disk_size_gb = 10         # Minimum disk size
+    machine_type = "e2-small"
+    disk_size_gb = 10
     disk_type    = "pd-standard"
     oauth_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
 
-    # Ensure no external IP is assigned to node
     metadata = {
       "disable-legacy-endpoints" = "true"
     }
@@ -86,7 +70,6 @@ resource "google_container_node_pool" "cheap_pool" {
     tags = ["no-external-ip"]
   }
 
-  # Prevent external IP assignment to nodes
   management {
     auto_upgrade = true
     auto_repair  = true
@@ -147,7 +130,7 @@ resource "kubernetes_service" "nginx" {
       target_port = 80
     }
 
-    type = "ClusterIP"  # Use ClusterIP to avoid external IP cost
+    type = "ClusterIP"
   }
 }
 
@@ -156,17 +139,21 @@ resource "kubernetes_service" "nginx" {
 resource "google_storage_bucket" "gcsfirst" {
   name          = "harsh_the_code_bucket"
   location      = "asia-south1"
-  
   public_access_prevention = "enforced"
 }
-########################## VM ###############################################
+
+########################## VM with Static IP ##########################################
+
+resource "google_compute_address" "static_ip" {
+  name   = "new-static-ip"
+  region = "asia-south1"
+}
 
 resource "google_service_account" "default" {
   account_id   = "terraform-vm-sa"
   display_name = "terraform-gcp-sa VM Instance"
 }
 
-# VM instance configuration with static external IP
 resource "google_compute_instance" "confidential_instance" {
   name         = "first-instance"
   zone         = "asia-south1-a"
@@ -177,8 +164,8 @@ resource "google_compute_instance" "confidential_instance" {
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-12"
-      size  = 10 # Disk size in GB (minimum for Persistent Disk)
-      type  = "pd-standard" # Standard Persistent Disk (cheapest option)
+      size  = 10
+      type  = "pd-standard"
       labels = {
         my_label = "value"
       }
@@ -189,9 +176,8 @@ resource "google_compute_instance" "confidential_instance" {
     network    = google_compute_network.vpc_network.id
     subnetwork = google_compute_subnetwork.subnetwork.id
 
-    # Use the provided static external IP for SSH access
     access_config {
-      nat_ip = "34.93.86.66"
+      nat_ip = google_compute_address.static_ip.address
     }
   }
 
@@ -200,44 +186,76 @@ resource "google_compute_instance" "confidential_instance" {
     scopes = ["cloud-platform"]
   }
 
-  # Add metadata to allow SSH keys
-  metadata = {
+   metadata = {
     ssh-keys = " "
-      }
+    # Add a startup script to install Kubernetes and Apache
+    metadata_startup_script = <<-EOT
+      #!/bin/bash
+
+      # Update and install necessary packages
+      apt-get update -y
+      apt-get upgrade -y
+
+      # Install Docker (required by Kubernetes)
+      apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+      curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+      add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+      apt-get update -y
+      apt-get install -y docker-ce
+
+      # Enable and start Docker
+      systemctl enable docker
+      systemctl start docker
+
+      # Install Kubernetes tools (kubectl, kubeadm, kubelet)
+      curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+      apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+      apt-get update -y
+      apt-get install -y kubeadm kubelet kubectl
+
+      # Mark Kubernetes packages to prevent accidental updates
+      apt-mark hold kubeadm kubelet kubectl
+
+      # Install Apache
+      apt-get install -y apache2
+      systemctl enable apache2
+      systemctl start apache2
+
+      # Output a message for debugging
+      echo "Startup script executed: Kubernetes and Apache installed" >> /var/log/startup-script.log
+    EOT
+  }
 }
-#################################################firewall_rules##################################################
-# Data source to get authenticated user's email via OpenID
-data "google_client_openid_userinfo" "me" {}
+
+########################## Firewall Rules ##########################################
 
 resource "google_compute_firewall" "allow_ssh" {
   name    = "allow-ssh"
-  network = google_compute_network.vpc_network.name  # Use your network if different
+  network = google_compute_network.vpc_network.name
 
   allow {
     protocol = "tcp"
     ports    = ["22"]
   }
 
-  source_ranges = ["0.0.0.0/0"]  # Allow SSH traffic from anywhere
+  source_ranges = ["0.0.0.0/0"]
 
-  target_tags = ["ssh-server"]  # Ensure your VM is tagged with "ssh-server"
+  target_tags = ["ssh-server"]
 }
 
 resource "google_compute_firewall" "allow_http_server" {
   name    = "allow-http-server"
-  network = google_compute_network.vpc_network.name  # Use your network if different
+  network = google_compute_network.vpc_network.name
 
   allow {
     protocol = "tcp"
     ports    = ["80", "443", "8001"]
   }
 
-  source_ranges = ["0.0.0.0/0"]  # Allow traffic from anywhere
+  source_ranges = ["0.0.0.0/0"]
 
-  target_tags = ["http-server"]  # You can tag your VM with "http-server"
+  target_tags = ["http-server"]
 }
-
-# New Firewall Rules to Allow All Connections to Node Group
 
 resource "google_compute_firewall" "allow_all_tcp_to_node_group" {
   name    = "allow-all-tcp-to-node-group"
@@ -247,9 +265,9 @@ resource "google_compute_firewall" "allow_all_tcp_to_node_group" {
     protocol = "tcp"
   }
 
-  source_ranges = ["0.0.0.0/0"]  # Allow all TCP traffic from anywhere
+  source_ranges = ["0.0.0.0/0"]
 
-  target_tags = ["no-external-ip"]  # Ensure your nodes are tagged with "no-external-ip"
+  target_tags = ["no-external-ip"]
 }
 
 resource "google_compute_firewall" "allow_all_udp_to_node_group" {
@@ -260,9 +278,9 @@ resource "google_compute_firewall" "allow_all_udp_to_node_group" {
     protocol = "udp"
   }
 
-  source_ranges = ["0.0.0.0/0"]  # Allow all UDP traffic from anywhere
+  source_ranges = ["0.0.0.0/0"]
 
-  target_tags = ["no-external-ip"]  # Ensure your nodes are tagged with "no-external-ip"
+  target_tags = ["no-external-ip"]
 }
 
 resource "google_compute_firewall" "allow_all_sctp_to_node_group" {
@@ -273,13 +291,14 @@ resource "google_compute_firewall" "allow_all_sctp_to_node_group" {
     protocol = "sctp"
   }
 
-  source_ranges = ["0.0.0.0/0"]  # Allow all SCTP traffic from anywhere
+  source_ranges = ["0.0.0.0/0"]
 
-  target_tags = ["no-external-ip"]  # Ensure your nodes are tagged with "no-external-ip"
+  target_tags = ["no-external-ip"]
 }
+
 resource "google_compute_firewall" "allow_egress" {
   name    = "allow-egress"
-  network = google_compute_network.vpc_network.name  # Use your network if different
+  network = google_compute_network.vpc_network.name
 
   direction = "EGRESS"
   priority  = 1000
@@ -288,8 +307,7 @@ resource "google_compute_firewall" "allow_egress" {
     protocol = "all"
   }
 
-  destination_ranges = ["0.0.0.0/0"]  # Allow all egress traffic to external networks
+  destination_ranges = ["0.0.0.0/0"]
 }
-
 
 ########################## End of Configuration ##############################
